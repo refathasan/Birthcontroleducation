4b1f1427a830b54e1c0075bbe355a725b193c6e4
refat hasan <refat.aiub@hotmail.com>
2022-10-13 06:23:03 +0600

    first commit

---
+++ app/src/main/java/com/refathasan/birthcontroleducation/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.refathasan.birthcontroleducation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, HomeNavActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
